bc9cce80aa2ad3684145019deadd4af26586fc75
MrsLecter <MissisLecter@protonmail.com>
2023-07-28 00:46:49 +0300

    add: base url

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,6 +1,6 @@
 import { ManufacturesNames } from "./manufactures-card-list";
 
-export const BASE_URL = "http://localhost:3000/";
+export const BASE_URL = "https://front-e-commerce-six.vercel.app/";
 
 export enum AppRoutes {
   Home = "/",
