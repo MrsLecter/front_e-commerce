ce51e9573b8696a57bb228ef76ca1af825e7fc43
MrsLecter <MissisLecter@protonmail.com>
2023-07-28 01:00:10 +0300

    fix: change about container width

--- src/components/common/wrappers/ContentWrapper.tsx
+++ src/components/common/wrappers/ContentWrapper.tsx
@@ -14,7 +14,7 @@ const StyledContentWrapper = styled.div`
   padding: 8px 16px;
   width: calc(100% - 16px);
   min-width: 230px;
-  max-width: 960px;
+  max-width: 994px;
   min-height: 777px;
   background-color: #fefeff;
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px;
