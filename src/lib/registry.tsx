02b1bb0353d8e6672fd43b26940fe0677c10181a
MrsLecter <MissisLecter@protonmail.com>
2023-07-28 00:06:28 +0300

    fix: fix nextjs13 bug with clearTag function

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -20,7 +20,7 @@ export default function StyledComponentsRegistry({
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
-    styledComponentsStyleSheet.instance.clearTag();
+    styledComponentsStyleSheet.instance;
     return <>{styles}</>;
   });
 
